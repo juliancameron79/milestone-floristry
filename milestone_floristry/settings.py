5c02a78ccad0f81629aea2d52a78047b60e8e469
juliancameron79 <juliancameron79@gmail.com>
2020-09-14 12:21:48 +0200

    Created media and static folders, updated settings.py and urls.py

--- milestone_floristry/settings.py
+++ milestone_floristry/settings.py
@@ -87,7 +87,7 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-ACCOUNT_AUTHENTICATIO_METHOD = 'username_email'
+ACCOUNT_AUTHENTICATIOn_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
@@ -143,6 +143,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
